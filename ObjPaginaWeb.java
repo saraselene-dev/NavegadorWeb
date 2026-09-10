4b8ff089a204d78b80f5a7f7d049c05beb0ddf62
saraselene-dev <saraselene@gmail.com>
2026-09-10 15:31:03 -0500

    Se elimina setFechaAcceso

--- ObjPaginaWeb.java
+++ ObjPaginaWeb.java
@@ -1,16 +1,40 @@
-import java.time.LocalDataTime;
+import java.time.LocalDateTime;
 
 public class ObjPaginaWeb {
     private String Url;
     private String Titulo;
-    private LocalDataTime FechaAcceso;
+    private LocalDateTime FechaAcceso;
     
     public ObjPaginaWeb() {
     }
 
-    public ObjPaginaWeb(String url, String titulo, LocalDataTime fechaAcceso) {
+    public ObjPaginaWeb(String url, String titulo) {
         Url = url;
         Titulo = titulo;
+        FechaAcceso = LocalDateTime.now();
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public LocalDateTime getFechaAcceso() {
+        return FechaAcceso;
+    }
+
+    public void setFechaAcceso(LocalDateTime fechaAcceso) {
         FechaAcceso = fechaAcceso;
     }
     

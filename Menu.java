ee7084f80025fde52e0f33493f6befa82e8ddc26
saraselene-dev <saraselene@gmail.com>
2026-09-15 00:31:35 -0500

    Navegador web con pilas: visitar, retroceder, avanzar y ver historial

--- Menu.java
+++ Menu.java
@@ -9,25 +9,37 @@ public class Menu {
         Metodos m = new Metodos();
 
         Stack<ObjPaginaWeb> atras = new Stack<>();
-        // Stack<ObjPaginaWeb> adelante = new Stack<>();
+        Stack<ObjPaginaWeb> adelante = new Stack<>();
 
         boolean continuar = true;
         System.out.println();
-        System.out.println("===Navegador S&J===");
+        System.out.println("===Navegador Web===");
 
         while (continuar) {
             System.out.println();
             System.out.println(" Ingrese una opción");
             System.out.println("1) Visitar Página");
-            System.out.println("2) Salir");
+            System.out.println("2) Página anterior");
+            System.out.println("3) Página siguiente");
+            System.out.println("4) Ver historial de navegación");
+            System.out.println("5) Salir");
             System.out.print("Opción: ");
-            int opt = sc.nextInt();
+            int opt = m.ValidarEntero(sc);
 
             switch (opt) {
                 case 1:
-                    atras = m.VisitarPagina(atras, sc);
+                    atras = m.VisitarPagina(atras, adelante, sc);
                     break;
                 case 2:
+                    atras = m.Retroceder(atras, adelante);
+                    break;
+                case 3:
+                    atras = m.Avanzar(atras, adelante);
+                    break;
+                case 4:
+                    m.VerHistorial(atras);
+                    break;
+                case 5:
                     continuar = false;
                     JOptionPane.showMessageDialog(null, "¡Hasta Luego!");
                     break;
--- Metodos.java
+++ Metodos.java
@@ -2,15 +2,55 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Metodos {
-    public Stack<ObjPaginaWeb> VisitarPagina(Stack<ObjPaginaWeb> historial, Scanner sc) {
+    public Stack<ObjPaginaWeb> VisitarPagina(Stack<ObjPaginaWeb> historial, Stack<ObjPaginaWeb> adelante, Scanner sc) {
         System.out.println("Ingrese la URL del sitio web");
         String url = ValidarString(sc);
         System.out.println("Ingrese el nombre de la Página Web");
         String titulo = ValidarString(sc);
         ObjPaginaWeb o = new ObjPaginaWeb(url, titulo);
         historial.push(o);
+        adelante.clear();
         System.out.println("Visitando: " + o.getTitulo());
         return historial;
+    }
+
+    public Stack<ObjPaginaWeb> Retroceder(Stack<ObjPaginaWeb> historial, Stack<ObjPaginaWeb> adelante) {
+        if (historial.isEmpty()) {
+            System.out.println("No hay páginas para retroceder");
+
+        } else {
+            ObjPaginaWeb actual = historial.pop();
+            adelante.push(actual);
+            System.out.println("Saliendo de: " + actual.getTitulo());
+            if (!historial.isEmpty()) {
+                ObjPaginaWeb siguiente = historial.peek();
+                System.out.println("Ahora en: " + siguiente.getTitulo());
+            } else {
+                System.out.println("No hay más páginas en el historial");
+            }
+        }
+        return historial;
+    }
+
+    public Stack<ObjPaginaWeb> Avanzar(Stack<ObjPaginaWeb> historial, Stack<ObjPaginaWeb> adelante) {
+        if (adelante.isEmpty()) {
+            System.out.println("No hay páginas para avanzar");
+        } else {
+            ObjPaginaWeb siguiente = adelante.pop();
+            historial.push(siguiente);
+            System.out.println("Avanzado a: " + siguiente.getTitulo());
+        }
+        return historial;
+    }
+
+    public void VerHistorial(Stack<ObjPaginaWeb> historial) {
+        for (int i = historial.size() - 1; i >= 0; i--) {
+            ObjPaginaWeb o = historial.get(i);
+            System.out.println("Titulo: " + o.getTitulo());
+            System.out.println("URL: " + o.getUrl());
+            System.out.println("Fecha de acceso: " + o.getFechaAcceso());
+            System.out.println("------------------------");
+        }
     }
 
     public String ValidarString(Scanner sc) {
@@ -22,4 +62,11 @@ public class Metodos {
         return entrada;
     }
 
+    public int ValidarEntero(Scanner sc) {
+        while (!sc.hasNextInt()) {
+            System.out.println("Ingrese un dato entero numérico");
+            sc.next();
+        }
+        return sc.nextInt();
+    }
 }
